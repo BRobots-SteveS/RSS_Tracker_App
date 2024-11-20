ab751e40d79e138bf8ee16d32dbb39aded8d8b4f
Schurmans Steve (Admin) <schustev901+BeJust@intra.just.fgov.be>
2024-11-20 09:21:25 +0100

    feat: Initial .Net8 Web API

---
+++ Rss_Tracking_Api/Program.cs
@@ -0,0 +1,36 @@
+using Rss_Tracking_Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Rss_TrackingDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:RssTrackerConnectionString"]);
+#if DEBUG
+    opts.EnableSensitiveDataLogging();
+#endif
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
