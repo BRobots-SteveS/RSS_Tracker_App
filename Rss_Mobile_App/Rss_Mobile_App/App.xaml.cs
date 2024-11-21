cebc8f3a0ae572a245e640e648543e98876da047
Schurmans Steve (Admin) <schustev901+BeJust@intra.just.fgov.be>
2024-11-21 14:37:38 +0100

    feat: Migrate From vanillla MAUI to UraniumUI MAUI

---
+++ Rss_Mobile_App/Rss_Mobile_App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using UraniumUI.Material.Resources;
+
+namespace Rss_Mobile_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Rss_Mobile_App/Rss_Mobile_App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Rss_Mobile_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
