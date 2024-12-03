60a38bb9a3e80f9052d942913a61f21c074ed8a4
Schurmans Steve (Admin) <schustev901+BeJust@intra.just.fgov.be>
2024-12-03 15:56:33 +0100

    feat: Network access added

---
+++ Rss_Mobile_App/Rss_Mobile_App/Services/MauiNavigationService.cs
@@ -0,0 +1,28 @@
+﻿namespace Rss_Mobile_App.Services
+{
+    public interface INavigationService
+    {
+        Task NavigateToAsync(string route, IDictionary<string, object>? routeParameters = null);
+        Task PopAsync();
+    }
+
+    public class MauiNavigationService : INavigationService
+    {
+        public MauiNavigationService() { }
+
+        //public Task InitializeAsync() =>
+        //    NavigateToAsync("PieOverviewView");
+
+        public Task NavigateToAsync(string route, IDictionary<string, object>? routeParameters = null)
+        {
+            var shellNavigation = new ShellNavigationState(route);
+
+            return routeParameters != null
+                ? Shell.Current.GoToAsync(shellNavigation, routeParameters)
+                : Shell.Current.GoToAsync(shellNavigation);
+        }
+
+        public Task PopAsync() =>
+            Shell.Current.GoToAsync("..");
+    }
+}
