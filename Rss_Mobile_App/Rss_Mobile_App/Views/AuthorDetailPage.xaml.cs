4030e920f327ca4e63c44b6f7e9fe5fca367355e
Schurmans Steve (Admin) <schustev901+BeJust@intra.just.fgov.be>
2024-12-08 23:29:23 +0100

    feat: adjustments to content pages

---
+++ Rss_Mobile_App/Rss_Mobile_App/Views/AuthorDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Rss_Mobile_App.ViewModels;
+
+namespace Rss_Mobile_App.Views;
+
+public partial class AuthorDetailPage : UraniumUI.Pages.UraniumContentPage
+{
+    public AuthorDetailPage(AuthorDetailViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+}
